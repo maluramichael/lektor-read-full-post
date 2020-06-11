06c499b394f182f59b733b32ac243403d0eb21dc
Michael Malura <michael@malura.de>
2020-06-11 14:30:05 +0200

    body_short is no only filled when theres are read more splitter in a post

--- lektor_read_full_post.py
+++ lektor_read_full_post.py
@@ -6,6 +6,7 @@ from copy import deepcopy
 
 from lektor.pluginsystem import Plugin
 
+print("LEKTOR READ FULL BLOG POST")
 
 class ReadFullPostPlugin(Plugin):
     name = "Read Full Post"
@@ -32,17 +33,15 @@ class ReadFullPostPlugin(Plugin):
     @classmethod
     def rfp_process_post(cls, post):
         body_type = post.datamodel.field_map['body'].type.name
-        post._data['body_short'] = deepcopy(post._data['body'])
         text_full = post._data['body'].source
 
         split_text = cls.rfp_spilt_text(body_type)
         contains_split = split_text in text_full
         if contains_split:
             split = text_full.split(split_text)
+            post._data['body_short'] = deepcopy(post._data['body'])
             post._data['body_short'].source = split[0]
             post._data['body'].source = '  \n'.join(split)
-
-        if cls.rfp_always_display() or contains_split:
             post._data['body_short'].source += cls.rfp_link_text(body_type, post.url_path)
 
         return post
@@ -50,4 +49,4 @@ class ReadFullPostPlugin(Plugin):
     def on_setup_env(self, **extra):
         ReadFullPostPlugin.rfp_config = self.get_config()
 
-        self.env.jinja_env.globals.update(plugin_read_full_post=ReadFullPostPlugin.rfp_process_post)
+        self.env.jinja_env.globals.update(read_full_post=ReadFullPostPlugin.rfp_process_post)
